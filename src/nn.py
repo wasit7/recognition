14ac47b6a5636c2220cfc0184b0e78b2d3425825
wasit7 <wasit7@gmail.com>
2014-12-28 12:17:36 +0700

    Fixed fft shift

--- src/nn.py
+++ src/nn.py
@@ -13,6 +13,7 @@ from scipy.spatial import kdtree
 import cPickle
 from scipy.ndimage import filters
 import sys
+import scipy.ndimage
 sys.setrecursionlimit(10000)
 
 rootdir="dataset_7"
@@ -20,6 +21,40 @@ wd=40
 
 def G(x,mu,s):
     return 1.0/ np.sqrt(2.0*np.pi)*np.exp(((x-mu)**2)/(-2.0*s**2))
+def normFFT(im_file):
+    im=np.array(Image.open(im_file).convert('L'))
+    #frequency domain
+    #f=np.log(np.abs(np.fft.fftshift(np.fft.fft2(im))))
+    f=np.log(np.abs(np.fft.fft2(im)))
+    #scaling
+    s=(200./f.shape[0],200./f.shape[1])
+    #normalized frequency domian
+    return scipy.ndimage.zoom(f,s,order = 2)
+    
+def getHash3(im_file):
+    ci=10
+    f=normFFT(im_file)
+    
+    rmax,cmax=f.shape    
+    sg=np.zeros((2*wd,wd-ci))
+    
+    sg[0:wd,:]=f[rmax-wd:rmax,ci:wd]
+    sg[wd:2*wd,:]=f[0:wd,ci:wd]
+    filters.gaussian_filter(sg, (4,4), (0,0), sg)
+    sg=np.reshape(sg,(1,-1))[0]
+    sg.astype(np.float32)
+    return sg
+    fsg=np.zeros(wd)
+    for b in xrange(wd):
+        for r in xrange(wd):
+            for c in xrange(wd):
+                rad=np.sqrt(r**2+c**2)            
+                fsg[b]=fsg[b]+sg[r,c]*G(rad,float(b),0.2)
+        fsg[b]=fsg[b]/(np.pi*float(b+1.0))
+        fsg=fsg/np.linalg.norm(fsg)
+        fsg.astype(np.float32)
+    return fsg
+    
 def getHash2(im_file):
     ci=10
     im = np.array(Image.open(im_file).convert('L'))
@@ -98,7 +133,7 @@ def find3(tree,f,all_files):
         print "%03d dis: %.3f %s"%(index,d[i],all_files[index])
 def findall(tree,all_files):
     for f in all_files:
-        fsg=getHash2(f)
+        fsg=getHash3(f)
         d,ids=tree.query(fsg,k=10)
         plt.figure(1)  
         for i,index in enumerate(ids):
@@ -111,45 +146,47 @@ def findall(tree,all_files):
         plt.set_cmap('gray')    
         plt.show()        
         plt.ginput(1)
-# read image to array
 
-all_files=[]
-for root, dirs, files in os.walk(rootdir):
-    for f in files:
-            if f.endswith('jpg') or f.endswith('JPG'):
-                all_files.append(os.path.join(root,f))
-#    for subdir in dirs:
-#        for iroot,idirs,ifiles in os.walk(os.path.join(root,subdir)):
-#            for f in ifiles:
-#                if f.endswith('jpg'):
-#                    all_files.append(os.path.join(iroot,f))
-# patch module-level attribute to enable pickle to work
-kdtree.node = kdtree.KDTree.node
-kdtree.leafnode = kdtree.KDTree.leafnode
-kdtree.innernode = kdtree.KDTree.innernode
-####construct tree
-j=0;
-end=0
-while end is not 1:
-    sub_bsg=[]
-    sub_files=[]
-    for i in xrange(2000):
-        if len(all_files) is 0:
-            end=1;            
-            break
-        f=all_files.pop(0)
-        sub_files.append(f)    
-        print '%02d %s'%(i,f)
-        bsg=getHash2(f)    
-        sub_bsg.append(bsg)
-    tree = kdtree.KDTree(sub_bsg)
-    pickleFile = open('%s/tree%02d.dat'%(rootdir,j), 'wb')
-    cPickle.dump((sub_files,tree,wd), pickleFile, cPickle.HIGHEST_PROTOCOL)
+if __name__ == '__main__':
+    # read image to array
+    
+    all_files=[]
+    for root, dirs, files in os.walk(rootdir):
+        for f in files:
+                if f.endswith('jpg') or f.endswith('JPG'):
+                    all_files.append(os.path.join(root,f))
+    #    for subdir in dirs:
+    #        for iroot,idirs,ifiles in os.walk(os.path.join(root,subdir)):
+    #            for f in ifiles:
+    #                if f.endswith('jpg'):
+    #                    all_files.append(os.path.join(iroot,f))
+    # patch module-level attribute to enable pickle to work
+    kdtree.node = kdtree.KDTree.node
+    kdtree.leafnode = kdtree.KDTree.leafnode
+    kdtree.innernode = kdtree.KDTree.innernode
+    ####construct tree
+    j=0;
+    end=0
+    while end is not 1:
+        sub_bsg=[]
+        sub_files=[]
+        for i in xrange(2000):
+            if len(all_files) is 0:
+                end=1;            
+                break
+            f=all_files.pop(0)
+            sub_files.append(f)    
+            print '%02d %s'%(i,f)
+            bsg=getHash3(f)    
+            sub_bsg.append(bsg)
+        tree = kdtree.KDTree(sub_bsg)
+        pickleFile = open('%s/tree%02d.dat'%(rootdir,j), 'wb')
+        cPickle.dump((sub_files,tree,wd), pickleFile, cPickle.HIGHEST_PROTOCOL)
+        pickleFile.close()
+        j=j+1
+    ####load tree
+    pickleFile = open('%s/tree%02d.dat'%(rootdir,0), 'rb')
+    (all_files,tree,wd) = cPickle.load(pickleFile)
     pickleFile.close()
-    j=j+1
-####load tree
-#pickleFile = open('tree.pic', 'rb')
-#(all_files,tree,wd) = cPickle.load(pickleFile)
-#pickleFile.close()
-#findall(tree,all_files)
+    findall(tree,all_files)
 
--- src/nn_find.py
+++ src/nn_find.py
@@ -10,11 +10,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.spatial import kdtree
 import cPickle
-from nn import getHash2
+from nn import getHash3
 import os
 def findall(tree,all_files):
     for f in all_files:
-        fsg=getHash2(f)
+        fsg=getHash3(f)
         d,ids=tree.query(fsg,k=10)
         plt.figure(1)  
         for i,index in enumerate(ids):
@@ -34,7 +34,7 @@ def findall2(all_trees,all_files):
     for files in all_files:
         for f in files:
             temp={'ds':[],'ids':[],'files':[]}
-            fsg=getHash2(f)
+            fsg=getHash3(f)
             for tree in all_trees:
                 ds,ids=tree.query(fsg,k)
                 temp['ds'].append(ds)
@@ -56,55 +56,57 @@ def findall2(all_trees,all_files):
             plt.set_cmap('gray')    
             plt.show()        
             plt.ginput(1)
-# patch module-level attribute to enable pickle to work
-kdtree.node = kdtree.KDTree.node
-kdtree.leafnode = kdtree.KDTree.leafnode
-kdtree.innernode = kdtree.KDTree.innernode
-
-####load tree
-all_treesfiles=[]
-for root, dirs, files in os.walk('.'):
-    for f in files:
-            if f.endswith('dat'):
-                all_treesfiles.append(os.path.join(root,f))
-
-all_trees=[]
-all_files=[]
-for tf in all_treesfiles:
-    pickleFile = open(tf, 'rb')
-    files,tree,wd = cPickle.load(pickleFile)
-    pickleFile.close()
-    all_trees.append(tree)
-    all_files.append(files)
-#findall2(all_trees,all_files)
-k=10    
-for files in all_files:
-    for f in files:
-        
-        temp={'ds':[],'ids':[],'files':[]}
-        fsg=getHash2(f)
-        for sub in xrange(len(all_trees)):
-            ds,ids=all_trees[sub].query(fsg,k)
+            
+if __name__ == '__main__':
+    # patch module-level attribute to enable pickle to work
+    kdtree.node = kdtree.KDTree.node
+    kdtree.leafnode = kdtree.KDTree.leafnode
+    kdtree.innernode = kdtree.KDTree.innernode
+    
+    ####load tree
+    all_treesfiles=[]
+    for root, dirs, files in os.walk('.'):
+        for f in files:
+                if f.endswith('dat'):
+                    all_treesfiles.append(os.path.join(root,f))
+    
+    all_trees=[]
+    all_files=[]
+    for tf in all_treesfiles:
+        pickleFile = open(tf, 'rb')
+        files,tree,wd = cPickle.load(pickleFile)
+        pickleFile.close()
+        all_trees.append(tree)
+        all_files.append(files)
+    #findall2(all_trees,all_files)
+    k=10    
+    for files in all_files:
+        for f in files:
+            
+            temp={'ds':[],'ids':[],'files':[]}
+            fsg=getHash3(f)
+            for sub in xrange(len(all_trees)):
+                ds,ids=all_trees[sub].query(fsg,k)
+                for i in xrange(k):
+                    temp['ds'].append(ds[i])
+                    temp['ids'].append(ids[i])
+                    temp['files'].append(all_files[sub][ids[i]])
+    
+            id_sort=np.argsort(temp['ds'])
+            sort_d=np.array(temp['ds'])[id_sort[0:k]]
+            sort_ids=np.array(temp['ids'])[id_sort[0:k]]
+            sort_files=[]
             for i in xrange(k):
-                temp['ds'].append(ds[i])
-                temp['ids'].append(ids[i])
-                temp['files'].append(all_files[sub][ids[i]])
-
-        id_sort=np.argsort(temp['ds'])
-        sort_d=np.array(temp['ds'])[id_sort[0:k]]
-        sort_ids=np.array(temp['ids'])[id_sort[0:k]]
-        sort_files=[]
-        for i in xrange(k):
-            sort_files.append(temp['files'][id_sort[i]])
-        plt.figure(1)  
-        for i in xrange(k):
-            print "%03d dis: %.3f %s"%(sort_ids[i],sort_d[i],sort_files[i])
-            plt.subplot(2,5,i+1)
-            plt.title("%03d dis: %.3f"%(sort_ids[i],sort_d[i]),fontsize=10)
-            plt.xlabel('%s'%sort_files[i])            
-            im_icon=np.array(Image.open(sort_files[i]).convert('L'))
-            plt.imshow(im_icon)
-            plt.axis('off')
-        plt.set_cmap('gray')    
-        plt.show()        
-        plt.ginput(1)
+                sort_files.append(temp['files'][id_sort[i]])
+            plt.figure(1)  
+            for i in xrange(k):
+                print "%03d dis: %.3f %s"%(sort_ids[i],sort_d[i],sort_files[i])
+                plt.subplot(2,5,i+1)
+                plt.title("%03d dis: %.3f"%(sort_ids[i],sort_d[i]),fontsize=10)
+                plt.xlabel('%s'%sort_files[i])            
+                im_icon=np.array(Image.open(sort_files[i]).convert('L'))
+                plt.imshow(im_icon)
+                plt.axis('off')
+            plt.set_cmap('gray')    
+            plt.show()        
+            plt.ginput(1)
